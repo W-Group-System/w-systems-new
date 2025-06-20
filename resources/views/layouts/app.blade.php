8a20e1ed8611398487693dc5ff33fa980126e17f
JunJihad <junjihadbarroga@gmail.com>
2025-06-20 17:10:53 +0800

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,78 +78,28 @@
     z-index: -1;
 }
 
-.main-content {
-    position: relative;
-    z-index: 1;
+.video-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    min-width: 100vw;
     min-height: 100vh;
-    color: white; /
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    padding: 2rem;
-    background: rgba(0, 0, 0, 0.4); Optional: dark overlay
+    width: auto;
+    height: auto;
+    z-index: -1;
+    object-fit: cover;
+    background-size: cover;
 }
+
 
 </style>
 <body class="g-sidenav-show   bg-gray-300">
     <div id = "myDiv" style="display:none;" class="loader"></div>
-    <div class="min-height-300 bg-primary position-absolute w-100"></div>
-    {{-- <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-        <div class="sidenav-header">
-          <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-          <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-            <img src="./assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
-          </a>
-        </div>
-        <hr class="horizontal dark mt-0">
-        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-          <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link {{ Request::is('departments') ? 'active' : '' }}" href="{{ url('/departments') }}">
-                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="ni ni-tag text-primary text-sm opacity-10"></i>
-                  </div>
-                  <span class="nav-link-text ms-1">Departments</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link {{ Request::is('system_setup') ? 'active' : '' }}" href="{{ url('/system_setup') }}">
-                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="ni ni-settings-gear-65 text-primary text-sm opacity-10"></i>
-                  </div>
-                  <span class="nav-link-text ms-1">Systems Setup</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link {{ Request::is('all_systems') ? 'active' : '' }}" href="{{ url('/all_systems') }}">
-                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                    <i class="ni ni-bold-right text-primary text-sm opacity-10"></i>
-                  </div>
-                  <span class="nav-link-text ms-1">All Systems</span>
-                </a>
-              </li>
-              @if ($departments)
-                @foreach($departments as $department)
-                <li class="nav-item">
-                  <a class="nav-link {{ Request::is('system/' . $department->name . '/' . $department->id) ? 'active' : '' }}" href="{{ url('system/' . $department->name . '/' . $department->id) }}">
-                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                      <i class="ni ni-bold-right text-primary text-sm opacity-10"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">{{ $department->name }}</span>
-                  </a>
-                </li>
-              @endforeach
-              @endif
-          </ul>
-        </div>
-      </aside> --}}
+    {{-- <div class="min-height-300 bg-primary position-absolute w-100"></div> --}}
     <main class="main-content position-relative border-radius-lg ">
-       <video autoplay muted loop playsinline class="video-background">
-        <source src="{{ asset('assets/img/bg.mp4') }}" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
+        <video autoplay muted loop playsinline class="video-background">
+            <source src="{{ asset('assets/img/bg.mp4') }}" type="video/mp4">
+        </video>
         <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
             <div class="container-fluid py-1 px-3">
               <nav aria-label="breadcrumb">
