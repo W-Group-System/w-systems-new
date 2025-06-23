cb1e1120cd6089d2f3202f8a30c79f41ab0da365
JunJihad <junjihadbarroga@gmail.com>
2025-06-23 16:36:52 +0800

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'W Systems') }}</title>
 
     <!-- Scripts -->
     {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <link rel="shortcut icon" href="{{asset('images/front-logo.png')}}">
+    <link rel="shortcut icon" href="{{asset('/assets/img/w-logo.webp')}}">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" type="text/css">
